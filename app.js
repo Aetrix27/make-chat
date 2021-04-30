a7f7be53bc81a4351a6f28bf7c72da14881da9f9
Aetrix27 <davedogg323@gmail.com>
2021-04-30 15:10:38 -0700

    Users can log in

--- app.js
+++ app.js
@@ -5,15 +5,18 @@ const server = require('http').Server(app);
 
 //Socket.io
 const io = require('socket.io')(server);
+//app.js
 io.on("connection", (socket) => {
-  console.log("🔌 New user connected! 🔌");
+  // This file will be read on new socket connections
+  require('./sockets/chat.js')(io, socket);
 })
 
-//app.js
 //Express View Engine for Handlebars
 const exphbs  = require('express-handlebars');
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+
 //Establish your public folder
 app.use('/public', express.static('public'))
 
